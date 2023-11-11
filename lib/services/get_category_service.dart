275a4dd5c4efbcbfbc85c4858af02437947eff79
ismail-24 <ismailtarek888@gmail.com>
2023-11-11 14:43:31 +0200

    Create get_category_service.dart

---
+++ lib/services/get_category_service.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:store/models/product_model.dart';
+
+class CategoriesService {
+  Future<List<ProductModel>> getCategoriesProducts(
+      {required String categoryName}) async {
+    http.Response respons = await http.get(
+      Uri.parse('https://fakestoreapi.com/products/category/$categoryName'),
+    );
+    List<dynamic> data = jsonDecode(respons.body);
+    List<ProductModel> productsList = [];
+    for (int i = 0; i < data.length; i++) {
+      productsList.add(
+        ProductModel.fromJson(
+          data[i],
+        ),
+      );
+    }
+    return productsList;
+  }
+}
